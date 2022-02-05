d061ed5fcb3a4cccf38776f53ab001f831814854
root <root@localhost>
2022-02-05 09:18:27 +0530

    auto publish 1644032907182

---
+++ src/main.py
@@ -0,0 +1,12 @@
+print("enter your name:")
+a=input()
+print("hello",a)
+name=input()
+if name.count > 12 :
+   print("tu chutiya hai")
+ elif:
+   print("tu chutiya tha")
+ else:
+   print("tu abhi bhi chutiya hai")
+  
+   
